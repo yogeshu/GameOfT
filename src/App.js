1a16f22b29bf9502ef3ce70abf137d1afe0baac2
yogeshu <yogeshbhavsar1994@gmail.com>
2021-06-11 11:52:08 +0530

    Added package Papaparse to deal with CSV rendering and Added Anysc function to fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Papa from "papaparse";
+import "./App.css";
+import Header from "./components/Header";
+import TheMain from "./components/TheMain";
+import Footer from "./components/Footer";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const response = await fetch("/Asset/battles.csv");
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv text
+      const results = Papa.parse(csv, { header: true }); // object with { data, errors, meta }
+      const rows = results.data; // array of objects
+      setData(rows);
+      setLoading(false);
+    }
+    getData();
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="App">
-     App 
+      <div className="app"></div>
+      <Header />
+      <TheMain />
+      <Footer />
     </div>
   );
 }
